b1c5cce39bf2db5c1ef079aafc61a8d704cac3b8
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2023-12-08 01:16:56 +0100

    extra 8 dec

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'collector'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'collector', '*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
