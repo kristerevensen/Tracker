cd57e46ab51dfe4d135539b19144a8f423879ec4
Krister Ross <k@mcminn.no>
2024-08-31 15:10:52 +0200

    310824

--- app/Http/Controllers/CollectorController.php
+++ app/Http/Controllers/CollectorController.php
@@ -2,35 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\DataPage;
 use Illuminate\Http\Request;
-use App\Models\Analytics;
-use App\Models\Conversion;
+use App\Models\DataPage;
 use App\Models\DataLinkClicks;
 use App\Models\FormSubmission;
-use App\Models\LinkClicks;
-use App\Models\Project;
 use Illuminate\Support\Str;
 
 class CollectorController extends Controller
 {
-    //
-    public function index()
+    public function index(Request $request)
     {
-        // return view("");
-    }
-    public function generateUniqueId($length = 10)
-    {
-        return str::random($length);
-    }
-    function generateUrlCode($url) {
-        return md5($url);
-    }
-
-
-    public function store(Request $request)
-    {
-        $eventType = $request->input('eventType');
+        $eventType = $request->query('eventType');
 
         switch ($eventType) {
             case 'pageLoad':
@@ -42,82 +24,45 @@ class CollectorController extends Controller
             case 'formSubmit':
                 return $this->handleFormSubmit($request);
 
-            case 'conversion':  // Ny case for konverteringer
-                return $this->handleConversion($request);
-
             default:
                 return response()->json(['error' => 'Unknown event type'], 400);
         }
     }
 
-
-    protected function handleConversion(Request $request)
-{
-    $projectCode = $request->input('project_code');
-    if (!$this->validateProjectCode($projectCode)) {
-        return response()->json(['error' => 'Invalid project code'], 400);
-    }
-
-    try {
-        // Lagre konverteringsdataene
-        $conversion = new Conversion;
-
-        $conversion->session_id = $request->input('session_id');
-        $conversion->project_code = $projectCode;
-        $conversion->conversion_type = $request->input('conversionType');
-        $conversion->conversion_value = $request->input('conversionValue');
-        $conversion->page_url = $request->input('pageUrl');
-        $conversion->referrer = $request->input('referrer');
-        $conversion->timestamp = now();
-
-        // Lagre konverteringsdata
-        $conversion->save();
-
-        return response()->json(['message' => 'Conversion data stored successfully'], 201);
-    } catch (\Exception $e) {
-        return response()->json(['error' => 'Failed to store conversion data', 'details' => $e->getMessage()], 500);
-    }
-}
-
-
-
     protected function handlePageLoad(Request $request)
     {
-        $projectCode = $request->input('project_code');
+        $projectCode = $request->query('project_code');
         if (!$this->validateProjectCode($projectCode)) {
             return response()->json(['error' => 'Invalid project code'], 400);
         }
+
         try {
-            // Create a new Analytics instance
             $analytics = new DataPage;
 
-            // Assign data from request to the analytics model
-            $analytics->url = $request->input('url');
-            $analytics->event_type = $request->input('eventType');
-            $analytics->content_hash = $request->input('contentHash');
-            $analytics->page_content = $request->input('pageContent');
-            $analytics->url_code = $this->generateUrlCode($request->input('url'));
-            $analytics->title = $request->input('title');
-            $analytics->referrer = $request->input('referrer');
-            $analytics->entrance = $this->determineEntrance($request->input('session_id'));
-            $analytics->device_type = $request->input('device_type');
-            $analytics->project_code = $request->input('project_code');
-            $analytics->session_id = $request->input('session_id');
-            $analytics->hostname = $request->input('hostname');
-            $analytics->protocol = $request->input('protocol');
-            $analytics->pathname = $request->input('pathname');
-            $analytics->language = $request->input('language');
-            $analytics->cookie_enabled = $request->input('cookie_enabled') ? 1 : 0;
-            $analytics->screen_width = $request->input('screen_width');
-            $analytics->screen_height = $request->input('screen_height');
-            $analytics->history_length = $request->input('history_length');
-            $analytics->word_count = $request->input('word_count');
-            $analytics->form_count = $request->input('form_count');
-            $analytics->meta_description = $request->input('meta_description');
-            $analytics->outbound_links = serialize($request->input('outbound_links'));
-            $analytics->inbound_links = serialize($request->input('inbound_links'));
+            $analytics->url = $request->query('url');
+            $analytics->event_type = $request->query('eventType');
+            $analytics->content_hash = $request->query('contentHash');
+            $analytics->page_content = $request->query('pageContent');
+            $analytics->url_code = $this->generateUrlCode($request->query('url'));
+            $analytics->title = $request->query('title');
+            $analytics->referrer = $request->query('referrer');
+            $analytics->device_type = $request->query('device_type');
+            $analytics->project_code = $request->query('project_code');
+            $analytics->session_id = $request->query('session_id');
+            $analytics->hostname = $request->query('hostname');
+            $analytics->protocol = $request->query('protocol');
+            $analytics->pathname = $request->query('pathname');
+            $analytics->language = $request->query('language');
+            $analytics->cookie_enabled = $request->query('cookie_enabled') ? 1 : 0;
+            $analytics->screen_width = $request->query('screen_width');
+            $analytics->screen_height = $request->query('screen_height');
+            $analytics->history_length = $request->query('history_length');
+            $analytics->word_count = $request->query('word_count');
+            $analytics->form_count = $request->query('form_count');
+            $analytics->meta_description = $request->query('meta_description');
+            $analytics->outbound_links = serialize(explode(',', $request->query('outbound_links')));
+            $analytics->inbound_links = serialize(explode(',', $request->query('inbound_links')));
 
-            // Save the analytics data
             $analytics->save();
 
             return response()->json(['message' => 'Analytics data stored successfully'], 201);
@@ -128,30 +73,28 @@ class CollectorController extends Controller
 
     protected function handleLinkClick(Request $request)
     {
-        $projectCode = $request->input('project_code');
+        $projectCode = $request->query('project_code');
         if (!$this->validateProjectCode($projectCode)) {
             return response()->json(['error' => 'Invalid project code'], 400);
         }
+
         try {
-            // Create a new instance for link click data
             $linkClick = new DataLinkClicks;
 
-            // Assign data from the request to the link click model
-            $linkClick->session_id = $request->input('session_id');
-            $linkClick->event_type = $request->input('eventType');
-            $linkClick->project_code = $request->input('project_code');
-            $linkClick->link_url = $request->input('linkUrl');
-            $linkClick->url_code = $this->generateUrlCode($request->input('linkUrl'));
-            $linkClick->link_text = $request->input('linkText');
-            $linkClick->click_class = $request->input('clickClass');
-            $linkClick->click_id = $request->input('clickId');
-            $linkClick->data_attributes = is_array($request->input('dataAttributes')) ? serialize($request->input('dataAttributes')) : null;
-            $linkClick->page_url = $request->input('pageUrl');
-            $linkClick->click_type = $request->input('clickType');
-            $linkClick->coordinates_x = $request->input('coordinates.x');
-            $linkClick->coordinates_y = $request->input('coordinates.y');
+            $linkClick->session_id = $request->query('session_id');
+            $linkClick->event_type = $request->query('eventType');
+            $linkClick->project_code = $projectCode;
+            $linkClick->link_url = $request->query('linkUrl');
+            $linkClick->url_code = $this->generateUrlCode($request->query('linkUrl'));
+            $linkClick->link_text = $request->query('linkText');
+            $linkClick->click_class = $request->query('clickClass');
+            $linkClick->click_id = $request->query('clickId');
+            $linkClick->data_attributes = is_array($request->query('dataAttributes')) ? serialize($request->query('dataAttributes')) : null;
+            $linkClick->page_url = $request->query('pageUrl');
+            $linkClick->click_type = $request->query('clickType');
+            $linkClick->coordinates_x = $request->query('coordinates.x');
+            $linkClick->coordinates_y = $request->query('coordinates.y');
 
-            // Save the link click data
             $linkClick->save();
 
             return response()->json(['message' => 'Link click data stored successfully'], 201);
@@ -162,24 +105,21 @@ class CollectorController extends Controller
 
     protected function handleFormSubmit(Request $request)
     {
-        $projectCode = $request->input('project_code');
+        $projectCode = $request->query('project_code');
         if (!$this->validateProjectCode($projectCode)) {
             return response()->json(['error' => 'Invalid project code'], 400);
         }
 
         try {
-            // Create a new instance for form submission data
             $formSubmission = new FormSubmission;
 
-            // Assign data from the request to the form submission model
-            $formSubmission->session_id = $request->input('session_id');
+            $formSubmission->session_id = $request->query('session_id');
             $formSubmission->project_code = $projectCode;
-            $formSubmission->form_id = $request->input('formDetails.id');
-            $formSubmission->form_name = $request->input('formDetails.name');
-            $formSubmission->page_url = $request->input('pageUrl');
-            $formSubmission->form_data = json_encode($request->input('formDetails.elements'));
+            $formSubmission->form_id = $request->query('formDetails.id');
+            $formSubmission->form_name = $request->query('formDetails.name');
+            $formSubmission->page_url = $request->query('pageUrl');
+            $formSubmission->form_data = json_encode($request->query('formDetails.elements'));
 
-            // Save the form submission data
             $formSubmission->save();
 
             return response()->json(['message' => 'Form submission data stored successfully'], 201);
@@ -188,22 +128,13 @@ class CollectorController extends Controller
         }
     }
 
-
     protected function validateProjectCode($projectCode)
     {
-        if (!Project::where('project_code', $projectCode)->exists()) {
-            return false;
-        }
-        return true;
+        return Project::where('project_code', $projectCode)->exists();
     }
 
-
-    protected function determineEntrance($sessionID)
+    protected function generateUrlCode($url)
     {
-        // Check if the session ID exists in the database
-        return DataPage::where('session_id', $sessionID)->doesntExist() ? 1 : 0;
+        return md5($url);
     }
-
-
-
-    }
+}
